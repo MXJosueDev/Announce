449ed013ce97c9f049fed67b0207f7adfd81c169
MXJosueDev <69768362+MXJosueDev@users.noreply.github.com>
2020-12-15 13:31:40 -0600

    Update Announce.php

--- src/MXJosueDev/Announce/Announce.php
+++ src/MXJosueDev/Announce/Announce.php
@@ -3,3 +3,15 @@
 namespace MXJosueDev\Announce;
 
 use pocketmine\{plugin\PluginBase, Server, Player, command\CommandSender, command\Command};
+
+class Announce extends PluginBase {
+  
+  public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args){
+    switch($cmd->getName){
+      case "announce":
+        if($sender instanceof Player){
+          
+        }
+    }
+  }
+}
