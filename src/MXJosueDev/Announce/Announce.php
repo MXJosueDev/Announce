95825de522e08cc84e5573d8d77cf3de0ad73965
MXJosueDev <69768362+MXJosueDev@users.noreply.github.com>
2020-12-15 14:00:34 -0600

    Update Announce.php

--- src/MXJosueDev/Announce/Announce.php
+++ src/MXJosueDev/Announce/Announce.php
@@ -2,16 +2,29 @@
 
 namespace MXJosueDev\Announce;
 
-use pocketmine\{plugin\PluginBase, Server, Player, command\CommandSender, command\Command};
+use pocketmine\{plugin\PluginBase, Server, Player, command\CommandSender, command\Command, utils\TextFormat as TF};
 
 class Announce extends PluginBase {
   
-  public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args){
+  public function onEnable(){
+    @mkdir($this->getDataFolder());
+    $this->saveDefaultConfig();
+    $this->saveResource("config.yml");
+  }
+  
+  public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
     switch($cmd->getName){
       case "announce":
-        if($sender instanceof Player){
+        if(count($args) == 0){
+          $sender->sendMessage(TF::RED."Usage /announce <message>");
+        }
+        if(count($args) >= 1){
+          $message = implode(" ", $args);
           
+          Server::getInstance()->broadcastMessage($this->getConfig()->get("prefix"));
+          Server::getInstance()->broadcastMessage(TF::GRAY.$message);
         }
     }
+    return true;
   }
 }
