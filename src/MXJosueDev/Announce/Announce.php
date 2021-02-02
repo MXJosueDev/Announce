896264c29e10128e67534fc5f02203e73a604892
MXJosueDev <69768362+MXJosueDev@users.noreply.github.com>
2021-02-01 18:41:01 -0600

    Update Announce.php

--- src/MXJosueDev/Announce/Announce.php
+++ src/MXJosueDev/Announce/Announce.php
@@ -1,43 +1,28 @@
 <?php
-
+declare(strict_types=1);
+/**
+ * Announce Plugin.
+ *
+ * Author: MXJosueDev
+ * Status: Public
+ * Contributors: None
+ */
 namespace MXJosueDev\Announce;
 
 use MXJosueDev\Announce\Commands\AnnounceCommand;
+
 use pocketmine\plugin\PluginBase;
-use pocketmine\{Server, Player};
-use pocketmine\command\Command;
 use pocketmine\utils\TextFormat as TF;
 
 class Announce extends PluginBase {
-  
-  /**
-   * @var Announce $plugin
-  */
-  public $plugin;
-  
-  /**
-   * @var Instance $instance
-  */
-  public static $instance;
-  
-  /**
-   * @return void
-  */
-  public function onLoad() : void{
-    self::$instance = $this;
-  }
-  
-  /**
-   * @return void
-  */
-  public function onEnable() : void{
-    $this->getServer()->getCommandMap()->register("announce", new AnnounceCommand($this));
-  }
-  
-  /**
-   * @return Announce
-  */
-  public static function getInstance() : self{
-    return self::$instance;
-  }
+	
+	/** @var Announce */
+	public static $plugin;
+	
+	/**
+	 * @return void
+	 */
+	public function onEnable() : void{
+		$this->getServer()->getCommandMap()->register("announce", new AnnounceCommand($this));
+	}
 }
